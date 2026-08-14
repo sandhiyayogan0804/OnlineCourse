ab9894730fa47348e57555b919fb507e5434483a
sandhiyayogan0804 <sandhiyayogan0804@gmail.com>
2026-08-14 22:56:40 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200)
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='lesson'
+    )
+    content = models.TextField()
+
+
+class Question(models.Model):
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE
+    )
+    question_text = models.CharField(max_length=200)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(
+        'Enrollment',
+        on_delete=models.CASCADE
+    )
+    choices = models.ManyToManyField(Choice)
+
+
+class Enrollment(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE
+    )
