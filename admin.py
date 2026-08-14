101e4dc01ea2ea9b63dc4fc7e483290ff7b0b1dd
sandhiyayogan0804 <sandhiyayogan0804@gmail.com>
2026-08-14 22:59:40 +0530

    Set up admin interface for Course, Lesson, Question
    Register models with Django admin and define inlines for Question and Lesson.

---
+++ admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+from .models import (
+    Course,
+    Lesson,
+    Instructor,
+    Learner,
+    Question,
+    Choice,
+    Submission,
+)
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
